342fccbe2ab37acb9bcbb746f256afe2eeec9331
kocoedwards <79869557+kocoedwards@users.noreply.github.com>
2021-03-12 10:52:48 -0800

    Add files via upload

---
+++ BestRapperPythonProject.py
@@ -0,0 +1,10 @@
+bestRapper = input("Who is the greatest rapper of all time?")
+
+
+if bestRapper == "Travis Scott":
+    print("You know what? I also think {} is the best rapper alive!".format(best.Rapper))
+
+else:
+    print("Huh. That's odd, I never thought {} was that good...".format(bestRapper))
+    
+    
