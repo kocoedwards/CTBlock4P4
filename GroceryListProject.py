939973ad21ed2db47548d2a80f0f670becbb2460
kocoedwards <79869557+kocoedwards@users.noreply.github.com>
2021-04-23 10:57:13 -0700

    Add files via upload

---
+++ GroceryListProject.py
@@ -0,0 +1,23 @@
+guacList = []
+salsaList = ["whole tomatoes", "jalapeno", "pepper"]
+
+print(guacList)
+
+guacList.append("avocados")
+
+guacList.append("onion")
+
+guacList.append("limes")
+
+guacList.append("garlic powder")
+
+guacList.append("salt")
+
+guacList.append("tortilla chips")
+
+guacList.append("cilantro")
+
+guacList.extend(salsaList)
+
+
+
